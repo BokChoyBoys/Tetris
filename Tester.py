6b105055418b355c2afeefcf1162a2df32611461
BokChoyBoys <45873162+BokChoyBoys@users.noreply.github.com>
2019-06-02 03:04:41 -0700

    Add files via upload

--- Tester.py
+++ Tester.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, division, print_function
 from tkinter import *
 import numpy as np
+import time
+import math
+from PIL import Image, ImageTk
 #import pyautogui
 import random
 from PIL import Image, ImageTk
@@ -12,84 +15,188 @@ class Shape:
     def __init__(self,shape):
         return
 
+def newshape(event):
+    if rowfilled(19):
+        print("nice")
+    global shapes,dy,dx,shapetype,board
+    for x in range(4):
+        board[shapepoints[shapetype][x] // 4 + dy][shapepoints[shapetype][x] % 4 + dx + 3] = -1
+    dy = 0
+    dx = 0
+    shapetype = random.randint(0,6)
+    shapes = []
+    for x in range(4):
+        shapes.append(
+            canvas.create_image((shapepoints[shapetype][x] % 4 + 3) * size, shapepoints[shapetype][x] // 4 * size,
+                                anchor=NW, image=shapesimg[shapetype]))
+        board[shapepoints[shapetype][x] // 4][shapepoints[shapetype][x] % 4 + 3] = shapetype + 1
 def rowfilled(row):
-    for x in range(len(row)):
+    for x in range(boardwidth):
         if board[row][x] == 0:
             return False
     return True
 
 def move(event):
-    global i,down,shift,degree
-    #shapecoords = np.array((3 + shift) * size, size * (down)),((7 + shift) * size, (down + 1) * size)
-    canvas.delete(i)
+    global i,dy,dx,degree
     if event.keysym == "Up":
-        #shapecoords = shapecoords * ((0,-1),(-1,0))
-        degree += 1
+        return
     elif event.keysym == "Down":
         #down another if
-        if down < boardheight - 2:
-            downinput()
-            down += 1
+        if valid(1):
+            downinput(pressed = True)
+        else:
+            newshape(event)
     elif event.keysym == "Left":
-        if shift > -3:
+        if valid(2):
             leftinput()
-            shift -= 1
     elif event.keysym == "Right":
-        if shift < 3:
+        if valid(3):
             rightinput()
-            shift += 1
-    i = canvas.create_rectangle(((3 + shift) * size, size * (down)),((7 + shift) * size, (down + 1) * size),fill="#01f0f1")
-    #i = canvas.create_rectangle(((4 + shift) * size, size *down),((6 + shift) * size, (down + 2) * size),fill="#f0f001")
-    print(board)
-def downinput():
+
+def downinput(pressed):
+    global dy
+    if not valid(1):
+        newshape(None)
+        dy -= 1
+    for s in shapes:
+        canvas.delete(s)
+    dy += 1
+
     for x in range(4):
-        board[down + 1][shift+ x+3] = board[down][shift + x+3]
-        board[down][shift + x+3] = 0
+        board[shapepoints[shapetype][x] // 4 + dy - 1][shapepoints[shapetype][x] % 4 + dx + 3] = 0
+
+    for x in range(4):
+        board[shapepoints[shapetype][x] // 4 + dy][shapepoints[shapetype][x] % 4 + dx + 3] = shapetype + 1
+
+    for x in range(4):
+         shapes[x] = canvas.create_image((shapepoints[shapetype][x] % 4 + dx + 3) * size, (shapepoints[shapetype][x] // 4 + dy) * size, anchor=NW,
+                             image=shapesimg[shapetype])
+    if not pressed:
+        win.after(delay,downinput,False)
+    print(board)
 
 def leftinput():
+    global dx
+    for s in shapes:
+        canvas.delete(s)
+    dx -= 1
+
     for x in range(4):
-        board[down][shift + 2 + x] = board[down][shift + 3 + x]
-        board[down][shift + 3 + x] = 0
+        board[shapepoints[shapetype][x] // 4 + dy][shapepoints[shapetype][x] % 4 + dx + 1+ 3] = 0
+
+    for x in range(4):
+        board[shapepoints[shapetype][x] // 4 + dy][shapepoints[shapetype][x] % 4 + dx + 3] = shapetype + 1
+
+    for x in range(4):
+        shapes[x] = canvas.create_image((shapepoints[shapetype][x] % 4 + dx + 3) * size,
+                                            (shapepoints[shapetype][x] // 4 + dy) * size, anchor=NW,
+                                            image=shapesimg[shapetype])
+    print(board)
 
 def rightinput():
+    global dx
+    for s in shapes:
+        canvas.delete(s)
+    dx += 1
+
     for x in range(4):
-        board[down][7 + shift - x] = board[down][6 + shift - x]
-        board[down][6 + shift - x] = 0
+        board[shapepoints[shapetype][x] // 4 + dy][shapepoints[shapetype][x] % 4 + dx - 1 + 3] = 0
+
+    for x in range(4):
+        board[shapepoints[shapetype][x] // 4 + dy][shapepoints[shapetype][x] % 4 + dx + 3] = shapetype + 1
+
+    for x in range(4):
+        shapes[3 - x] = canvas.create_image((shapepoints[shapetype][3 - x] % 4 + dx + 3) * size,
+                                            (shapepoints[shapetype][3 - x] // 4 + dy) * size, anchor=NW,
+                                            image=shapesimg[shapetype])
+    print(board)
+
+def rotate(right):
+    if right:
+
+        return
+
+def valid(dir): #up,down,left,right
+    px = 0
+    py = 0
+    if dir == 0:
+        px = dx + 1
+        return
+    if dir == 1:
+        py = dy + 1
+        px = dx
+    if dir == 2:
+        px = dx - 1
+    if dir == 3:
+        px = dx + 1
+
+    a = 0
+    for x in range(4):
+        if shapepoints[shapetype][x] // 4 + py > boardheight - 1:
+            print("1")
+            return False
+        if shapepoints[shapetype][x] % 4 + px + 2 < -1 or shapepoints[shapetype][x] % 4 + px + 1 > 7:
+            print("2")
+            return False
+        if (dir == 0 or dir == 1) and board[shapepoints[shapetype][x] // 4 + py][shapepoints[shapetype][x] % 4 + 3 + dx] == -1:
+            print("3")
+            return False
+        if (dir == 2 or dir == 3) and board[shapepoints[shapetype][x] // 4 + dy][shapepoints[shapetype][x] % 4 + 3 + px] == -1:
+            print(px)
+            return False
+    return True
 
 boardwidth = 10
 boardheight = 20
 wwidth = 600
 wheight = 800
 size = wheight/boardheight
-gamestarted = True
-degree = 0
-rotationtuple = ()
-shift = 0
-down = 0
+gameplaying = True
+dx = 0
+dy = 0
 toplayerblock = False
+shapetype = random.randint(0,6)
+delay = 1000
+
 board = np.zeros((boardheight,boardwidth))
+
 win = Tk()
 win.title('Tetris')
 win.resizable(0,0)
 canvas = Canvas(win, width=wwidth, height = wheight,bg = "white",borderwidth=0,highlightthickness=0)
 win.bind('<Key>', move)
-#canvas.bind('<Button-3>',flag)
-#win.bind('<space>', restart)
+win.bind('<space>', newshape)
 canvas.pack()
 
 for y in range(boardheight):
     canvas.create_line(0,y*(wheight/boardheight), (wheight/boardheight) * (boardwidth),y*(wheight/boardheight))
 for x in range(boardwidth):
     canvas.create_line((x+1) * (wheight/boardheight),0,(x+1)*(wheight/boardheight),wheight)
-#if not toplayerblock: #change 0 to above
-i = canvas.create_rectangle((3 * wheight/boardheight,0,7 * wheight/boardheight, wheight/boardheight),fill = "black")
-#i = canvas.create_rectangle(((4 + shift) * size, size *down),((6 + shift) * size, (down + 2) * size),fill = "black")
+
+tiles = Image.open("tiles.jpg")
+tiles = tiles.resize((280,40), Image.ANTIALIAS)
+shapesimg = [
+    ImageTk.PhotoImage(tiles.crop((0,0,size,size))), #O [0]
+    ImageTk.PhotoImage(tiles.crop((size,0,size * 2,size))), #S [1]
+    ImageTk.PhotoImage(tiles.crop((size * 2,0,size * 3,size))), #T [2]
+    ImageTk.PhotoImage(tiles.crop((size * 3,0,size * 4,size))), #I [3]
+    ImageTk.PhotoImage(tiles.crop((size * 4, 0, size * 5, size))), #Z [4]
+    ImageTk.PhotoImage(tiles.crop((size * 5,0,size * 6,size))), #L [5]
+    ImageTk.PhotoImage(tiles.crop((size * 6,0,size * 7,size))), #J [6]
+    ]
+shapepoints = [[1,2,5,6], #same indices as above
+               [1,2,4,5],
+               [1,4,5,6],
+               [0,1,2,3],
+               [0,1,5,6],
+               [2,4,5,6],
+               [0,4,5,6],
+]
+shapes = []
 for x in range(4):
-    board[0][x+3] = 1
+    shapes.append(canvas.create_image((shapepoints[shapetype][x] % 4 + 3) * size,shapepoints[shapetype][x] // 4* size,anchor=NW,image= shapesimg[shapetype]))
+    board[shapepoints[shapetype][x] // 4][shapepoints[shapetype][x] % 4 + 3] = shapetype + 1
+#start()
+#win.after(delay,downinput,False)
 print(board)
-#green 00f000 S
-#blue 0101f0 L
-#orange efa000 J
-#red f00100 Z
-#pink a000f1 T
 win.mainloop()
